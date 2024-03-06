ffde1c7798446659f1464823e03a6c8e57465f4c
Biyun Zhu <zhuby85@gmail.com>
2024-03-06 10:22:04 -0700

    Create a poem object and add new image file

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ const poems = [
     author: "Ernie",
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
-  },  
+  },
+  {
+    author: "Jo",
+    poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
+    image: "redrose.webp"
+  }
 ];
 
 let displayPoems = (array) => {
